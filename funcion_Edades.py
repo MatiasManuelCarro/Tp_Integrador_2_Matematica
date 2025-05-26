d73a1b16062bdc2398c547bf03aad0025fd526af
Carro <matias.carro@sofse.gob.ar>
2025-05-26 11:06:57 -0300

    Funcion años

--- funcion_Edades.py
+++ funcion_Edades.py
@@ -3,16 +3,16 @@ from datetime import date
 fecha_actual = date.today()
 # utilizamos la fecha_actual para conseguir el año
 año_actual = fecha_actual.year
-
-años_nacimiento = [1975, 1986, 1987, 1992, 1993, 1996]
-#años_nacimiento = []
+#años_nacimiento = [1975, 1986, 1987, 1992, 1993, 1996]
+años_nacimiento = []
 edad_integrantes = []
 año_par = int(0)
 año_impar = int(0)
 grupo_z = True          #Comienza en True la funcion
 hay_bisiesto = False
-cantidad_años = int(6)         #solo para testeo
-#cantidad_años = int(0)      #version que tiene que ir 
+#cantidad_años = int(6)         #solo para testeo
+cantidad_años = int(0)      #version que tiene que ir 
+producto_cartesiano = []
 
 def funcion_ingreso_años():
     global cantidad_años        #contador de la cantidad de años ingresados
@@ -31,14 +31,14 @@ def funcion_ingreso_años():
 
 #funcion que llama a todas las funciones de operaciones con los años
 def funcion_operaciones_años():
-    global años_nacimiento, grupo_z,hay_bisiesto, cantidad_años
-    print(f"Lista de años: {años_nacimiento}")          #muestra la lista de años ingresados
-    for i in range(0, cantidad_años):           #recorre la lista de años y las carga en las funciones
-        año_par, año_impar = funcion_años_par_impar(años_nacimiento[i])
-        grupo_z = funcion_grupo_z(años_nacimiento[i])
-        hay_bisiesto = funcion_es_bisiesto(años_nacimiento[i])
+    global años_nacimiento, grupo_z, hay_bisiesto, cantidad_años, año_par, año_impar, años_nacimiento, edad_integrantes, año_actual, producto_cartesiano
+    for año in años_nacimiento:           #recorre la lista de años y las carga en las funciones
+        año_par, año_impar = funcion_años_par_impar(año, año_par, año_impar)
+        grupo_z = funcion_grupo_z(año, grupo_z)
+        hay_bisiesto = funcion_es_bisiesto(año, hay_bisiesto)
+    producto_cartesiano = funcion_producto_cartesiano()
 
-    print(f"En el grupo tenemos {año_par} integrantes que nacieron en año par y {año_impar} integrantes que nacieron en año impar")
+    print(f"\nEn el grupo tenemos {año_par} integrantes que nacieron en año par y {año_impar} integrantes que nacieron en año impar")
     if grupo_z:
         print(f"el Grupo es \"Grupo Z\" ")
     else:
@@ -47,34 +47,50 @@ def funcion_operaciones_años():
         print("Tenemos un año especial")
     menu()
 
-def funcion_años_par_impar(año):
-    global año_par, año_impar
+def funcion_años_par_impar(año, año_par, año_impar):
     if año % 2 == 0:
         año_par += 1
     else:
         año_impar += 1
     return año_par, año_impar
 
-def funcion_grupo_z(año):
-    global grupo_z
+def funcion_grupo_z(año, grupo_z):
     if año < 2000:      #si alguno de los años es menor a 2000, el grupo no es Z
         grupo_z = False     #Grupo z pasa a falso
     return grupo_z
 
-def funcion_es_bisiesto(año):
-    global hay_bisiesto
+def funcion_es_bisiesto(año, hay_bisiesto):
     if año % 4 == 0 and (año % 100 != 0 or año % 400 == 0):         #verifica que el año es bisiesto
         hay_bisiesto = True         #Si al menos un año es bisiesto, pasa a True     
     return hay_bisiesto
 
 def funcion_producto_cartesiano():
-    global años_nacimiento, edad_integrantes, año_actual
-    años_nacimiento.sort()          #ordenamos la lista ingresada
+    global años_nacimiento, año_actual, edad_integrantes
+    #ordenamos la lista ingresada de años
+    años_nacimiento.sort()         
     #calculamos la edad de los integrantes
-    for i in range(cantidad_años):
-        edad = año_actual - años_nacimiento[i]
-        print(edad)
-        edad_integrantes.append(edad)
+    for año in años_nacimiento:
+        edad_integrantes.append(año_actual - año)
+
+    #mostramos las listas de años y edades
+    print(f"La lista de años de nacimiento es: {años_nacimiento}")
+    print(f"La lista de las edades de los integrantes es: {edad_integrantes}")
+    
+    #ciclo que genera los conjuntos cartesiano
+    for año in años_nacimiento:
+        for edad in edad_integrantes:
+            producto_cartesiano.append((año, edad))
+    
+    #mostralos los productos
+    print("\nLos productos cartesianos entre los años y las edades son:")
+    for producto in producto_cartesiano:
+        print(producto)
+
+
+
+    
+    
+
     
 ###
 #def ingreso_automatico():
